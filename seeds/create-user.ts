41f595c9eab88d04b1962bcce04b29c0086fe156
Jacky Lai <jacky96923@gmail.com>
2023-12-27 20:32:43 +0800

    added knex seed

---
+++ seeds/create-user.ts
@@ -0,0 +1,11 @@
+import { Knex } from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+  // Deletes ALL existing entries
+  await knex("users").del();
+
+  // Inserts seed entries
+  await knex("users").insert([
+    { id: 1, username: "jacky96923@gmail.com", password: "s2008079" },
+  ]);
+}
